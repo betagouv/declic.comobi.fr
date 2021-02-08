29a228f075e48288b2ce0f7147f83778cfc1e4cc
audrey bramy <audrey.bramy@gmail.com>
2021-02-08 17:53:47 +0100

    remove useless files for the instance

--- geography/tripString.js
+++
@@ -1,3 +0,0 @@
-export default function({origin, destination, waypoints = []}){
-    return [origin, ...waypoints, destination].join(' - ')
-}
--- spreadsheetDatabase/requestStatusConstants.js
+++
@@ -1,2 +0,0 @@
-export const REQUEST_STATUS_KEY = 'Trajet effectué Oui/Non/Régulier/Fuck'
-export const REQUEST_STATUS_NO_RELEVANT_DRIVER_VALUE = 'N'
